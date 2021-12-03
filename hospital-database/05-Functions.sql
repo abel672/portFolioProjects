f7fcb46a17213363ce59bcae2e1547a5f29a05db
Abel Mescua <abelmescua@gmail.com>
2021-12-03 13:55:43 +0100

    Functions

---
+++ hospital-database/05-Functions.sql
@@ -0,0 +1,18 @@
+DELIMITER $$
+CREATE FUNCTION tableExists (_tableName nvarchar(255))
+RETURNS BOOLEAN
+BEGIN
+	IF (SELECT COUNT(*) FROM information_schema.tables
+		WHERE table_schema = DATABASE()
+		AND table_name = 'clinic_historic1') = 1
+	THEN
+		RETURN TRUE;
+	ELSE
+		RETURN FALSE;
+        END IF;
+END
+$$
+
+-- get patient's phone number
+
+DELIMITER ;
